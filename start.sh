058998bff47c3eb96210a25a54320e38681427ee
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2019-08-21 15:11:28 -0500

    DEVX-819 remove error in start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,3 @@
 /usr/local/bin/dub template /etc/confluent/docker/kafka.properties.template /etc/kafka/kafka-streams.properties
 java $JAVA_OPTS $KAFKA_OPTS -jar /app/cp-demo-kstreams.jar /etc/kafka/kafka-streams.properties
 
-FADFDFDF
